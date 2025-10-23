e16c4c8c7c3d2ad02bf91295192073b7fb2ab6df
Rushikesh Dilip Patil <rushipatil12448@gmail.com>
2025-10-23 11:42:05 +0530

    adding feature

---
+++ server/routes/UserRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+import User from '../models/User';
+
+const app = express();
+app.use(express.json()); 
+
+app.post('/register', async (req, res) => {
+    try {
+        const user = await User.create(req.body); // Creating a user with the data from the request body
+        res.json(user); // Responding with the created user
+    } catch (err) {
+        res.status(500).json({ message: 'Error creating user', error: err }); // Handling errors
+    }
+});
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import itemRoutes from "./routes/itemRoutes.js";
+import userRoutes from "./routes/UserRoutes.js";
 
 dotenv.config();
 
@@ -12,12 +13,14 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/items", itemRoutes);
+app.use("/api/user", userRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB connected"))
-  .catch((err) => console.error("❌ MongoDB Error:", err));
+  .then(() => console.log(" MongoDB connected"))
+  .catch((err) => console.error(" MongoDB Error:", err));
 
 app.listen(process.env.PORT || 5000, () =>
   console.log(`🚀 Server running on port ${process.env.PORT || 5000}`)
 );
+
