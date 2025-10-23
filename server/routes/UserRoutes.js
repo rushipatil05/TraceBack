741f2258b36fefbe060e8fd121fc660782933de7
Rushikesh Dilip Patil <rushipatil12448@gmail.com>
2025-10-23 12:08:22 +0530

    final signup

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -1,16 +1,15 @@
 import express from "express";
-import User from '../models/User.js';
+import User from "../models/User.js";
 
-const app = express();
-app.use(express.json()); 
+const router = express.Router();
 
-app.post('/register', async (req, res) => {
-    try {
-        const user = await User.create(req.body); // Creating a user with the data from the request body
-        res.json(user); // Responding with the created user
-    } catch (err) {
-        res.status(500).json({ message: 'Error creating user', error: err }); // Handling errors
-    }
+router.post("/register", async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: "Error creating user", error: err });
+  }
 });
 
 export default router;
