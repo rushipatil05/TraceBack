75b4c13c9d96c1f2b4f2ec9a4d257a0bbc86efff
Rushikesh Dilip Patil <rushipatil12448@gmail.com>
2025-10-23 20:06:11 +0530

    login fixed

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import User from "../models/User.js";
-import bcrypt from "bcrypt"; // for secure password hashing
+import bcrypt from "bcrypt"; 
 
 const router = express.Router();
 
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post("/register", async (req, res) => {
   try {
     const { name, email, password } = req.body;
-    
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = await User.create({ name, email, password: hashedPassword });
